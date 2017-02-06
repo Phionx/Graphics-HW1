c4c0e05bcd9197c0ad257bc6bd57c8a2264227fa
Phionx <shanjha.usa@gmail.com>
2017-02-05 20:10:26 -0500

    cool

--- image.py
+++ image.py
@@ -6,10 +6,11 @@ from sys import argv
 print "Generating Image\n"
 file = open('image.ppm', 'w+')
 file.truncate()
-file.write("P3 250 250 255\n")
-for i in range(0, 250):
-    for j in range (0, 250):
-        file.write("255 " + str(i%256) + " " + str(j%256) + " ")
+file.write("P3 500 500 255\n")
+
+for i in range(0, 500):
+    for j in range (0, 500):
+        file.write("" + str((i*i-j)%256) + " " + str((i+j)%256) + " " + str((j*j-i)%256) + " ")
     file.write("\n")
 
 print "done!"
